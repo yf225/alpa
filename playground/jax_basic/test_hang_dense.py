4b1ecd4e57f497c8138527eceab2f88274049a8b
Lianmin Zheng <lianminzheng@gmail.com>
2022-03-29 08:28:09 +0000

    add test_hang_conv.py and test_hang_dense.py

--- playground/jax_basic/test_hang_dense.py
+++ playground/jax_basic/test_hang_dense.py
@@ -1,19 +1,18 @@
 """Test auto sharding with convoluton nets."""
 from typing import Any
 
-from flax import linen as nn, optim
+from flax import linen as nn
 from flax.training.train_state import TrainState
 import jax
 import jax.numpy as jnp
 import numpy as np
 import optax
 
-from alpa import parallelize, global_config
+from alpa import parallelize
 from alpa.util import count_communication_primitives
 
-batch_size = 32
-image_size = 32
-channel = 512
+batch_size = 64
+hidden_size = 1024
 use_bias = True
 
 
@@ -21,17 +20,9 @@ class Model(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        last_x = None
         for i in range(10):
-            x = nn.Conv(features=channel,
-                        kernel_size=(3, 3),
-                        strides=(1, 1),
-                        use_bias=use_bias)(x)
+            x = nn.Dense(hidden_size, use_bias=use_bias)(x)
             x += jnp.sum(x)
-            #x = nn.relu(x)
-            #if last_x is not None:
-            #    x = x + last_x
-            #last_x = x
         return x
 
 @parallelize
@@ -46,8 +37,8 @@ def train_step(state, batch):
     return new_state
 
 batch = {
-    "x": jnp.ones((batch_size, image_size, image_size, channel)),
-    "y": jnp.ones((batch_size, image_size, image_size, channel))
+    "x": jnp.ones((batch_size, hidden_size)),
+    "y": jnp.ones((batch_size, hidden_size))
 }
 
 
